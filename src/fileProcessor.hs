e33c1f8944e847fd8f8bf48562534ae443138c43
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2022-11-17 06:49:37 +0700

    Restrict the types of images that can be processed

--- src/fileProcessor.hs
+++ src/fileProcessor.hs
@@ -2,36 +2,40 @@ module Src.FileProcessor where
 
 import qualified Data.ByteString as Byte
 
+-- | Given a byte string, return the number given by byte offset and byte size.
 readLittleEndian :: Byte.ByteString -> Int -> Int -> Integer
 readLittleEndian bytestring at 1 = toInteger $ Byte.index bytestring at
-readLittleEndian bytestring at n = (toInteger $ Byte.index bytestring at) + (readLittleEndian bytestring (at + 1) (n - 1)) * 256
+readLittleEndian bytestring at n = (toInteger $ Byte.index bytestring at) + (readLittleEndian bytestring (at + 1) (n - 1)) * 0x100
 
--- Returns the position of the first occurence of a pixel in the given bytestring
+-- | Returns the position of the first occurence of a pixel in the given bytestring.
 pixelArrayStart :: Byte.ByteString -> Int
-pixelArrayStart bytestring = fromIntegral $ readLittleEndian bytestring 10 4
+pixelArrayStart bytestring = fromIntegral $ readLittleEndian bytestring 0xA 4
 
--- Returns the width of the image in pixels
+-- | Returns the width of the image in pixels
 imageWidth :: Byte.ByteString -> Int
-imageWidth bytestring = fromIntegral $ readLittleEndian bytestring 18 4
+imageWidth bytestring = fromIntegral $ readLittleEndian bytestring 0x12 4
 
--- Returns the height of the image in pixels
+-- | Returns the height of the image in pixels
 imageHeight :: Byte.ByteString -> Int
-imageHeight bytestring = fromIntegral $ readLittleEndian bytestring 22 4
+imageHeight bytestring = fromIntegral $ readLittleEndian bytestring 0x16 4
 
--- Returns the number of bytes per pixel
-bytesPerPixel :: Byte.ByteString -> Int
-bytesPerPixel bytestring = fromIntegral $ readLittleEndian bytestring 28 2
-
--- Obtain the red, green, and blue bitmasks from the header
+-- | Obtain the red, green, and blue bitmasks from the header.
 bitmasks :: Byte.ByteString -> (Integer, Integer, Integer)
-bitmasks bytestring = (readLittleEndian bytestring 54 4, readLittleEndian bytestring 58 4, readLittleEndian bytestring 62 4)
+bitmasks bytestring = (
+  readLittleEndian bytestring 0x36 4,
+  readLittleEndian bytestring 0x3A 4,
+  readLittleEndian bytestring 0x3E 4)
 
--- Verify that the file is a valid bitmap file with supported headers
+-- | Verify that the file is a valid bitmap file with supported headers and bits per pixel.
 verifyBitmap :: Byte.ByteString -> Bool
-verifyBitmap bytestring = (byteAt 0 == 66) && (byteAt 1 == 77) &&
-  (  byteAt14 == 40 -- BITMAPV2HEADER
-  || byteAt14 == 52 -- BITMAPV3HEADER
-  || byteAt14 == 56 -- BITMAPV4HEADER
-  || byteAt14 == 108 -- BITMAPV5HEADER
-  ) where byteAt14 = byteAt 14
-          byteAt = Byte.index bytestring
+verifyBitmap bytestring = isBmp &&
+  (  bitsPerPixel == 16
+  || bitsPerPixel == 24
+  || bitsPerPixel == 32) && 
+  (  headerIndicatorByte == 52 -- BITMAPV2HEADER
+  || headerIndicatorByte == 56 -- BITMAPV3HEADER
+  || headerIndicatorByte == 108 -- BITMAPV4HEADER
+  || headerIndicatorByte == 124 -- BITMAPV5HEADER
+  ) where isBmp = readLittleEndian bytestring 0 2 == 0x4D42
+          bitsPerPixel = readLittleEndian bytestring 0x1C 2
+          headerIndicatorByte = readLittleEndian bytestring 0xE 4
