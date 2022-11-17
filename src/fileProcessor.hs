8220722a1d6a3c106fdbe9bc8040f83a023274c0
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2022-11-17 07:44:55 +0700

    More robust file validation system with Either

--- src/fileProcessor.hs
+++ src/fileProcessor.hs
@@ -2,6 +2,14 @@ module Src.FileProcessor where
 
 import qualified Data.ByteString as Byte
 
+data Metadata = Metadata {
+    width :: Int,
+    height :: Int,
+    offset :: Int,
+    bitsPerPixel :: Int,
+    bitsPerColor :: Int
+} deriving (Show)
+
 -- | Given a byte string, return the number given by byte offset and byte size.
 readLittleEndian :: Byte.ByteString -> Int -> Int -> Integer
 readLittleEndian bytestring at 1 = toInteger $ Byte.index bytestring at
@@ -11,13 +19,11 @@ readLittleEndian bytestring at n = (toInteger $ Byte.index bytestring at) + (rea
 pixelArrayStart :: Byte.ByteString -> Int
 pixelArrayStart bytestring = fromIntegral $ readLittleEndian bytestring 0xA 4
 
--- | Returns the width of the image in pixels
-imageWidth :: Byte.ByteString -> Int
-imageWidth bytestring = fromIntegral $ readLittleEndian bytestring 0x12 4
-
--- | Returns the height of the image in pixels
-imageHeight :: Byte.ByteString -> Int
-imageHeight bytestring = fromIntegral $ readLittleEndian bytestring 0x16 4
+-- | Returns the size of the image in width and height tuple.
+dimensions :: Byte.ByteString -> (Int, Int)
+dimensions bytestring = (imageWidth bytestring, imageHeight bytestring) where
+  imageWidth bytestring = fromIntegral $ readLittleEndian bytestring 0x12 4
+  imageHeight bytestring = fromIntegral $ readLittleEndian bytestring 0x16 4
 
 -- | Obtain the red, green, and blue bitmasks from the header.
 bitmasks :: Byte.ByteString -> (Integer, Integer, Integer)
@@ -26,16 +32,24 @@ bitmasks bytestring = (
   readLittleEndian bytestring 0x3A 4,
   readLittleEndian bytestring 0x3E 4)
 
--- | Verify that the file is a valid bitmap file with supported headers and bits per pixel.
-verifyBitmap :: Byte.ByteString -> Bool
-verifyBitmap bytestring = isBmp &&
-  (  bitsPerPixel == 16
-  || bitsPerPixel == 24
-  || bitsPerPixel == 32) && 
-  (  headerIndicatorByte == 52 -- BITMAPV2HEADER
-  || headerIndicatorByte == 56 -- BITMAPV3HEADER
-  || headerIndicatorByte == 108 -- BITMAPV4HEADER
-  || headerIndicatorByte == 124 -- BITMAPV5HEADER
-  ) where isBmp = readLittleEndian bytestring 0 2 == 0x4D42
-          bitsPerPixel = readLittleEndian bytestring 0x1C 2
-          headerIndicatorByte = readLittleEndian bytestring 0xE 4
+bitsPerPixel :: Byte.ByteString -> Either String Int
+bitsPerPixel bytestring = if bpp == 16 || bpp == 24 || bpp == 32 
+  then Right bpp
+  else Left "Unsupported bits per pixel" where
+    bpp = fromIntegral $ readLittleEndian bytestring 0x1C 2
+
+validateHeaderSize :: Byte.ByteString -> Either String ()
+validateHeaderSize bytestring = if 
+    (headerSize == 52 -- BITMAPV2INFOHEADER
+  || headerSize == 56 -- BITMAPV3INFOHEADER
+  || headerSize == 108 -- BITMAPV4HEADER
+  || headerSize == 124) -- BITMAPV5HEADER
+    then Right ()
+    else Left "Unsupported header size. This program only supports BITMAPV2INFOHEADER, BITMAPV3INFOHEADER, BITMAPV4HEADER, and BITMAPV5HEADER." where
+      headerSize = fromIntegral $ readLittleEndian bytestring 0xE 4
+
+validateProgramLength :: Byte.ByteString -> Either String ()
+validateProgramLength bytestring = if programLength < 0x36
+  then Left "File is too short to be a valid bitmap file."
+  else Right () where
+    programLength = Byte.length bytestring
