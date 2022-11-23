0e7ae893e6dc05ad8a8788e908140e08f5381341
Muhamad Nicky Salim <muhamadnickysalim@gmail.com>
2022-11-23 14:54:58 +0700

    Nicky: Fixing typo on data.vector, disable NN

--- main.hs
+++ main.hs
@@ -4,10 +4,43 @@ import qualified Data.ByteString as Byte
 import Src.UpscaleAlgorithm
 import Src.Processor
 import Src.Color
+import Src.Algorithms.NearestNeighbor
+import Data.Vector
 
 upscale :: UpscaleType -> Int -> String -> String -> IO ()
 upscale algorithm scale inputName outputName =
     Byte.readFile inputName >>= (return . (processWith algorithm scale)) >>= Byte.writeFile outputName
 
 main :: IO ()
-main = upscale NearestNeighbor 2 "example.bmp" "output.bmp"
+main = return ()
+
+exampleImg = Image 5 (Data.Vector.fromList [
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+
+  Color 255 255 255,
+  Color 0 255 255,
+  Color 255 255 255,
+  Color 0 255 255,
+  Color 255 255 255,
+
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+
+  Color 0 255 255,
+  Color 0 255 255,
+  Color 0 255 255,
+  Color 0 255 255,
+  Color 0 255 255,
+
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255,
+  Color 255 255 255])
--- src/upscaleAlgorithm.hs
+++ src/upscaleAlgorithm.hs
@@ -1,10 +1,13 @@
 module Src.UpscaleAlgorithm where
 import Src.Color
-import Vector
+import Data.Vector
 
 data UpscaleType = NearestNeighbor | ScaleX | HQX
 
-data Image = Image { width :: Int, pixels :: Vector Color }
+data Image = Image { width :: Int, pixels :: Vector Color } deriving Show
+
+height :: Image -> Int
+height image = (Data.Vector.length (pixels image)) `div` (width image)
 
 getColor:: Image -> Int -> Int -> Color
 getColor image x y = pixels image ! (y * width image + x)
