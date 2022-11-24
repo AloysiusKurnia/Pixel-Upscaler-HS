6e1bcd26063b3135ed98bec30ddafc4f5e49726f
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2022-11-24 11:17:13 +0700

    docs!

--- src/algorithms/common.hs
+++ src/algorithms/common.hs
@@ -3,7 +3,7 @@ module Src.Algorithms.Common where
 import qualified Graphics.Image as Img
 
 -- | If an integer is in the form of 2^m 3^n, return Just (m, n).
--- | Otherwise, return Nothing.
+-- Otherwise, return Nothing.
 factorizeTwoThree :: Int -> Maybe (Int, Int)
 factorizeTwoThree n
     | n `mod` 2 == 0 = factorizeTwoThree (n `div` 2) >>= (\(x, y) -> Just (x + 1, y))
@@ -11,16 +11,28 @@ factorizeTwoThree n
     | n == 1 = Just (0, 0)
     | otherwise = Nothing
 
+-- | A shorhand for RGB Image
 type RGBImage = Img.Image Img.VU Img.RGB Double
+
+-- | A shorthand for RGB Pixel
 type RGBPixel = Img.Pixel Img.RGB Double
+
+-- | A shorthand for a 2x2 square of any type a.
+-- It is ordered top left to top right, bottom left to bottom right.
 type Square2x2 a = (
     a, a,
     a, a)
+
+-- | A shorthand for a 3x3 square of any type a.
+-- It is ordered top left to top right, middle left to middle right, 
+-- bottom left to bottom right.
 type Square3x3 a = (
     a, a, a,
     a, a, a,
     a, a, a)
 
+-- | Returns the pixel at the given coordinates. If the coordinates are out of
+-- bounds, returns the pixel at the closest coordinates.
 safeGetPixel :: RGBImage -> (Int, Int) -> RGBPixel
 safeGetPixel img (x, y)
     | x < 0 = Img.index img (0, y)
@@ -29,6 +41,7 @@ safeGetPixel img (x, y)
     | y >= Img.rows img = Img.index img (x, Img.rows img - 1)
     | otherwise = Img.index img (x, y)
 
+-- | Returns the 3x3 neighborhood of the given pixel.
 getNeighborhood :: RGBImage -> (Int, Int) -> Square3x3 RGBPixel
 getNeighborhood img (x, y) = (
     safeGetPixel img (x - 1, y - 1),
@@ -43,11 +56,15 @@ getNeighborhood img (x, y) = (
     safeGetPixel img (x, y + 1),
     safeGetPixel img (x + 1, y + 1))
 
+-- | Merges an array of 2x2 squares into a rectangle with height of 2,
+-- represented by a tuple containing two arrays of the same length.
 stack2x2SquaresHorizontally :: [Square2x2 a] -> ([a], [a])
 stack2x2SquaresHorizontally [] = ([], [])
 stack2x2SquaresHorizontally ((a, b, c, d):xs) = (a:b:fst rest, c:d:snd rest)
     where rest = stack2x2SquaresHorizontally xs
 
+-- | Merges an array of 3x3 squares into a rectangle with height of 3,
+-- represented by a tuple containing three arrays of the same length.
 stack3x3SquaresHorizontally :: [Square3x3 a] -> ([a], [a], [a])
 stack3x3SquaresHorizontally [] = ([], [], [])
 stack3x3SquaresHorizontally ((a, b, c, d, e, f, g, h, i):xs) = (a:b:c:fst3 rest, d:e:f:snd3 rest, g:h:i:thd3 rest)
@@ -57,29 +74,38 @@ stack3x3SquaresHorizontally ((a, b, c, d, e, f, g, h, i):xs) = (a:b:c:fst3 rest,
         thd3 (_, _, x) = x
         rest = stack3x3SquaresHorizontally xs
 
+-- | Merges multiple rectangles with the height of 2 into a 2-dimensional array.
 stack2x2RowsVertically :: [([a], [a])] -> [[a]]
 stack2x2RowsVertically [] = []
 stack2x2RowsVertically ((a, b):xs) = a:b:stack2x2RowsVertically xs
 
+-- | Merges multiple rectangles with the height of 3 into a 2-dimensional array.
 stack3x3RowsVertically :: [([a], [a], [a])] -> [[a]]
 stack3x3RowsVertically [] = []
 stack3x3RowsVertically ((a, b, c):xs) = a:b:c:stack3x3RowsVertically xs
 
+-- | Creates a coordinate lattice of the given size.
 xySpace :: Int -> Int -> [[(Int, Int)]]
 xySpace width height = [[(x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
 
+-- | Applies a function returning a square to each element of a 2-dimensional array,
+-- expanding the array twice in width and height.
 apply2x2 :: (a -> Square2x2 b) -> [[a]] -> [[b]]
 apply2x2 f = stack2x2RowsVertically . map (stack2x2SquaresHorizontally . map f)
 
+-- | Applies a function returning a square to each element of a 2-dimensional array,
+-- expanding the array three times in width and height.
 apply3x3 :: (a -> Square3x3 b) -> [[a]] -> [[b]]
 apply3x3 f = stack3x3RowsVertically . map (stack3x3SquaresHorizontally . map f)
 
+-- | Upscales an image by a factor of 2, using the given upsampling function.
 upscale2x :: (RGBImage -> (Int, Int) -> Square2x2 RGBPixel) -> RGBImage -> RGBImage
 upscale2x algorithm img = Img.transpose 
     $ Img.fromLists 
     $ apply2x2 (algorithm img) 
     $ xySpace (Img.cols img) (Img.rows img)
 
+-- | Upscales an image by a factor of 3, using the given upsampling function.
 upscale3x :: (RGBImage -> (Int, Int) -> Square3x3 RGBPixel) -> RGBImage -> RGBImage
 upscale3x algorithm img = Img.transpose 
     $ Img.fromLists 
--- src/algorithms/scalex.hs
+++ src/algorithms/scalex.hs
@@ -3,6 +3,7 @@ module Src.Algorithms.ScaleX where
 import qualified Graphics.Image as Img
 import Src.Algorithms.Common
 
+-- | Given a pixel and its neighbors, scales the pixel twice.
 scale2xNeigborhood :: Square3x3 RGBPixel -> Square2x2 RGBPixel
 scale2xNeigborhood (a, b, c, d, e, f, g, h, i)
     | b /= h && d /= f = (
@@ -13,6 +14,7 @@ scale2xNeigborhood (a, b, c, d, e, f, g, h, i)
         if f == h then f else e)
     | otherwise = (e, e, e, e)
 
+-- | Given a pixel and its neighbors, scales the pixel three times.
 scale3xNeigborhood :: Square3x3 RGBPixel -> Square3x3 RGBPixel
 scale3xNeigborhood (a, b, c, d, e, f, g, h, i)
     | b /= h && d /= f = (
@@ -29,8 +31,10 @@ scale3xNeigborhood (a, b, c, d, e, f, g, h, i)
         if f == h then f else e)
     | otherwise = (e, e, e, e, e, e, e, e, e)
 
+-- | The scale2x algorithm.
 scale2x :: RGBImage -> (Int, Int) -> Square2x2 RGBPixel
 scale2x img (x, y) = scale2xNeigborhood (getNeighborhood img (x, y))
 
+-- | The scale3x algorithm.
 scale3x :: RGBImage -> (Int, Int) -> Square3x3 RGBPixel
 scale3x img (x, y) = scale3xNeigborhood (getNeighborhood img (x, y))
