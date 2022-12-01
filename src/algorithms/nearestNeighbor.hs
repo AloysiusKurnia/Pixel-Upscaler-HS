a8cb7f781a5edae93553c1cfe8ddefcf8a6b5094
Muhamad Nicky Salim <muhamadnickysalim@gmail.com>
2022-12-01 10:49:18 +0700

    Implement way new NN algorithm

--- src/algorithms/nearestNeighbor.hs
+++ src/algorithms/nearestNeighbor.hs
@@ -1,7 +1,20 @@
 module Src.Algorithms.NearestNeighbor where
 
-import Src.Color
-import Data.Vector
-import Src.UpscaleAlgorithm
+import qualified Graphics.Image as Img
+import Src.Algorithms.Common
 
--- Upscales an image using nearest neighbor algorithm
+-- | Given a pixel and its neighbors, scales the pixel twice.
+nn2xNeigborhood :: Square3x3 RGBPixel -> Square2x2 RGBPixel
+nn2xNeigborhood (_, _, _, _, e, _, _, _, _) = (e, e, e, e)
+
+-- | Given a pixel and its neighbors, scales the pixel three times.
+nn3xNeigborhood :: Square3x3 RGBPixel -> Square3x3 RGBPixel
+nn3xNeigborhood (_, _, _, _, e, _, _, _, _) = (e, e, e, e, e, e, e, e, e)
+
+-- | The nearest neighbor algorithm to scale an image 2x.
+nearestNeighbor2x :: RGBImage -> (Int, Int) -> Square2x2 RGBPixel
+nearestNeighbor2x img (x, y) = nn2xNeigborhood (getNeighborhood img (x, y))
+
+-- | The nearest neighbor algorithm to scale an image 3x.
+nearestNeighbor3x :: RGBImage -> (Int, Int) -> Square3x3 RGBPixel
+nearestNeighbor3x img (x, y) = nn3xNeigborhood (getNeighborhood img (x, y))
